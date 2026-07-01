3f70a4ef4aa2aebe2464604e810530ab3dbd6c16
shenryk <senhen123@gmail.com>
2026-07-01 21:21:41 +0300

    filled in correct answers wtih a few changes

--- .github/setup.sh
+++ .github/setup.sh
@@ -4,9 +4,9 @@ mkdir -p ~/.bitcoin
 cat <<EOF > ~/.bitcoin/bitcoin.conf
 [regtest]
 regtest=1
-rpcuser=user
-rpcpassword=password
-rpcport=18332
+rpcuser=polaruser
+rpcpassword=polaruser
+rpcport= http://127.0.0.1:18444
 EOF
 
 echo $(bitcoin-cli --version)
